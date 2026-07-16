f633d06482908c39a5ebdd35ffbd64f390d08aa9
Partha Paul <parthapaul2705@gmail.com>
2026-07-16 09:56:15 +0530

    feat(sql): create  table in sql with all attribute

--- Learn-MySQL/create-table.sql
+++ Learn-MySQL/create-table.sql
@@ -1,3 +1,11 @@
 create database learndb;
 use learndb ;
-create table users();
+
+create table users(
+    id INT AUTO_INCREMENT PRIMARY KEY  ,
+    name VARCHAR(100) NOT NULL ,
+    email VARCHAR(100) NOT NULL ,
+    gender ENUM('Male','Female','Other'),
+    date_of_birth DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
