132cd763e9c36426829d41799ad7346dd711d43c
Partha Paul <parthapaul2705@gmail.com>
2026-07-17 21:27:46 +0530

    feat(sql):Renaming a Table

--- Learn-MySQL/create-table.sql
+++ Learn-MySQL/create-table.sql
@@ -22,3 +22,17 @@ SELECT  * FROM users;
 -- select specific columns
 
 SELECT name, email FROM users;
+
+
+-- Renaming a Table
+
+-- To rename an existing table:
+
+RENAME TABLE users TO customers;
+
+-- To rename it back:
+
+RENAME TABLE customers TO users;
+
+
+SELECT  * FROM users;
