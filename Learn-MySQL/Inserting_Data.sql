f0df4d49649769337fabbad4511e7b0851bb1c67
Partha Paul <parthapaul2705@gmail.com>
2026-07-18 20:14:43 +0530

    feat(sql): insert data into  multiple rows

--- Learn-MySQL/Inserting_Data.sql
+++ Learn-MySQL/Inserting_Data.sql
@@ -11,6 +11,18 @@ CREATE TABLE emp (
 SELECT * FROM emp;
 INSERT INTO emp VALUES(1, 'Alice', 'alice@example.com', 'Female', '1995-05-14', DEFAULT);
 
--- insert data into specific columns
+-- insert data into specific columns ( its better ) use
 INSERT INTO emp (name, email, gender, date_of_birth) VALUES
 ('Bob', 'bob@example.com', 'Male', '1990-11-23');
+
+
+-- or for multiple rows:
+INSERT INTO emp (name, email, gender, date_of_birth) VALUES
+('Boc', 'bcs@example.com', 'Male', '1890-11-23'),
+('Charlie', 'charlie@example.com', 'Other', '1988-02-17');
+
+INSERT INTO emp (name, email, gender, date_of_birth) VALUES
+('Charlie', 'charlie@example.com', 'Other', '1988-02-17'),
+('David', 'david@example.com', 'Male', '2000-08-09'),
+('Eva', 'eva@example.com', 'Female', '1993-12-30');
+
